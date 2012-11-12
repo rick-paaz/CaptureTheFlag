73df29bd126b6185379c380db727592b96378e9d
mercer <mercer@cs.arizona.edu>
2012-11-12 15:31:45 -0700

    Added a comment to BearTrap

--- src/items/BearTrap.java
+++ src/items/BearTrap.java
@@ -3,6 +3,7 @@ package items;
 public class BearTrap extends Item {
 
   public BearTrap(String imageFileName) {
+    // Just added this comment after a Fetch and NOT a Synchronize
     super("BearTrap", imageFileName);
   }
 
