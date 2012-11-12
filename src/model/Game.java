db36436cc2efb74c0c4bd437031fae2c2f6e6e3c
mercer <mercer@cs.arizona.edu>
2012-11-12 14:49:02 -0700

    Deleted Player.java and made a format change to Game.  Just to see difference between git pull and git fetch and merge

--- src/model/Game.java
+++ src/model/Game.java
@@ -12,7 +12,6 @@ public class Game extends Observable {
     field = new GamePiece[Globals.ROWS][Globals.COLUMNS];
   }
 
- 
   // Also update the gamePiece location
   public void moveUnit(int fromRow, int fromColumn, int toRow, int toColumn) {
     if (outOfRange(fromRow, fromColumn) || outOfRange(toRow, toColumn))
@@ -20,28 +19,27 @@ public class Game extends Observable {
 
     field[toRow][toColumn] = field[fromRow][fromColumn];
     field[fromRow][fromColumn] = null;
-    
+
     field[toRow][toColumn].setPosition(toRow, toColumn);
- 
-    
-    ((Unit)field[toRow][toColumn]).chargeOneMoveCost();
-    
+
+    ((Unit) field[toRow][toColumn]).chargeOneMoveCost();
+
     setChanged();
     notifyObservers();
   }
 
   public Unit getUnit(int row, int column) {
-    return (Unit)field[row][column];
+    return (Unit) field[row][column];
   }
 
   // Also update the gamePiece location if the gemPiece is a unit
-  
+
   public void addPiece(GamePiece gamePiece, int row, int column) {
     if (outOfRange(row, column) || field[row][column] != null)
       throw new IllegalArgumentException();
-    
-    if(gamePiece instanceof Unit)
-    	gamePiece.setPosition(row, column);
+
+    if (gamePiece instanceof Unit)
+      gamePiece.setPosition(row, column);
 
     field[row][column] = gamePiece;
   }
@@ -55,19 +53,19 @@ public class Game extends Observable {
     String result = "";
     for (int r = 0; r < Globals.ROWS; r++) {
       for (int c = 0; c < Globals.COLUMNS; c++) {
-        
+
         String sideLetter = " ";
         Object o = field[r][c];
-        if( o instanceof Unit) {
-        if(((Unit)field[r][c]).getSide().equals(Globals.SIDE_LEFT))
+        if (o instanceof Unit) {
+          if (((Unit) field[r][c]).getSide().equals(Globals.SIDE_LEFT))
             sideLetter = "l";
-        else
-          sideLetter = "r";
+          else
+            sideLetter = "r";
         }
         if (field[r][c] == null)
           result += " . ";
         else
-          result += sideLetter+ field[r][c].getLetterRepresentation() + " ";
+          result += sideLetter + field[r][c].getLetterRepresentation() + " ";
       }
       result += "\n";
     }
